9c81c66c9fb87f2b0e19a2a139569e9a6ed6801f
juliedosher <juliegdosher@lewisu.edu>
2023-02-14 09:56:30 -0600

    added static folder

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.static(path.join(__dirname, 'static')));
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, './index.html'));
 });
+
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
